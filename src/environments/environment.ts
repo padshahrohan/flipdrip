53667825cb06850e5cc4a8f29bab62b9899c5887
mayanku <mayankguptajy1996@gmail.com>
2023-08-21 00:07:28 +0530

    Program rules finalized

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
    production: false,
-   contractAddress: '0x6Cd74B6b85720Bfa8f0747A8198F013cA44DBA84'
+   contractAddress: '0x0AD13f5015c27E9149B2C2a377563fAE6B44f027'
 };
